220b5ee4ff26a29a5cd28ae438f9f94116704094
Alumno Chacarilla <alumnotlsch@campustls.local>
2025-04-24 16:24:21 -0500

    se agregaron otras clases

---
+++ Cuadrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    internal class Cuadrado
+    {
+        private float b;
+        private float h;
+          
+
+        public Cuadrado(float b, float h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public float Area()
+        {
+            return b * h;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,11 +15,15 @@ namespace Clase
 
             Console.WriteLine(t1.Area());
 
-            Console.ReadLine();
+            Cuadrado c = new Cuadrado(5,7);
 
             new Rectangulo(6f,3f);
 
             new Circulo(3f);
+
+            Console.WriteLine("triangulo" + t2.Area());
+            Console.WriteLine("cuadrado" + c.Area());
+            Console.ReadLine();
 
         }
             
